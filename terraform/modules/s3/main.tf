f9198829b3f51a65711600a06a7140e3413e8ba3
takeshi8989 <htakeshi0614@gmail.com>
2024-11-18 15:32:08 -0800

    deploy

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -8,3 +8,8 @@ resource "aws_s3_object" "main_py" {
   source = "${path.root}/../main.py"
 }
 
+resource "aws_s3_object" "class_schedules_utc_json" {
+  bucket = aws_s3_bucket.iclicker_bucket.id
+  key    = "class_schedules_utc.json"
+  source = "${path.root}/../class_schedules_utc.json"
+}
